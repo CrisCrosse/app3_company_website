e9fd0a6959bb8924d87da350f753b2b76a09cda4
chris <chrisrossell1999@gmail.com>
2023-05-24 15:51:05 +0100

    Added the 3 columns with images, descriptions and names

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 st.title("The Best Company")
 st.write("""Consulted perpetual of pronounce me delivered. Too months nay end change relied who beauty wishes matter.
  Shew of john real park so rest we on. Ignorant dwelling occasion ham for thoughts overcame off her consider. Polite 
@@ -9,3 +11,29 @@ st.write("""Consulted perpetual of pronounce me delivered. Too months nay end ch
   age brother sending amongst why covered.
 """)
 
+st.header("Our Team")
+
+col1, blank_col1, col2, blank_col2, col3 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
+
+company_data = pandas.read_csv("data.csv")
+
+with col1:
+    for index, row in company_data[:4].iterrows():
+        name = row["first name"].capitalize() + " " + row["last name"].capitalize()
+        st.header(name)
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col2:
+    for index, row in company_data[4:8].iterrows():
+        name = row["first name"].capitalize() + " " + row["last name"].capitalize()
+        st.header(name)
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col3:
+    for index, row in company_data[8:].iterrows():
+        name = row["first name"].capitalize() + " " + row["last name"].capitalize()
+        st.header(name)
+        st.write(row["role"])
+        st.image("images/" + row["image"])
